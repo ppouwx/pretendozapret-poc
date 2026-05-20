cf7199a8d7396cb45f41ae8d8745bd9c7cc50af5
ppouwx <129798200+ppouwx@users.noreply.github.com>
2026-05-20 14:26:31 +0300

    Delete source directory

--- source/dns.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-bool DNS_Init(void);
-void DNS_Deinit(void);
-
-// Hooked getaddrinfo - returns false if real function should be called
-bool DNS_HandleGetAddrInfo(const char *node, void *hints, void *res);
